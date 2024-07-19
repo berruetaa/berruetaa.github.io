97ba04431f6edfc2aef85e0e01f18020db539536
berruetaa <berruetx@gmail.com>
2024-07-19 03:09:27 -0300

    hrrr

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,23 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const starField = document.getElementById('stars');
-    const numMeteors = 10;
+    const numStars = 200;  // Número total de estrellas
 
-    function createMeteor() {
-        const meteor = document.createElement('div');
-        meteor.classList.add('meteor');
-        // Generar una posición aleatoria en la parte superior o izquierda de la pantalla
-        if (Math.random() > 0.5) {
-            meteor.style.top = `${-10 - Math.random() * 10}vh`;
-            meteor.style.left = `${Math.random() * 100}vw`;
-        } else {
-            meteor.style.top = `${Math.random() * 100}vh`;
-            meteor.style.left = `${-10 - Math.random() * 10}vw`;
-        }
-        meteor.style.animationDelay = `${Math.random() * 5}s`;
-        starField.appendChild(meteor);
+    function createStar() {
+        const star = document.createElement('div');
+        star.classList.add('star');
+        star.style.top = `${Math.random() * 100}vh`;
+        star.style.left = `${Math.random() * 100}vw`;
+        star.style.animationDelay = `${Math.random() * 5}s`;
+        starField.appendChild(star);
     }
 
-    for (let i = 0; i < numMeteors; i++) {
-        createMeteor();
+    for (let i = 0; i < numStars; i++) {
+        createStar();
     }
 });
