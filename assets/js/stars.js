589b08a4512622ad7396ab70260880fd5f3d9136
berruetaa <berruetx@gmail.com>
2024-07-19 02:44:24 -0300

    update

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,32 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const starField = document.getElementById('stars');
-    const numStars = 100;  // Número total de estrellas
+    const numMeteors = 10;
 
-    function createStar() {
-        const star = document.createElement('div');
-        star.classList.add('star');
-        star.style.top = `${Math.random() * 100}vh`;
-        star.style.left = `${Math.random() * 100}vw`;
-        star.style.opacity = Math.random();
-        starField.appendChild(star);
-
-        // Cada estrella tiene una probabilidad de caer
-        if (Math.random() < 0.1) {
-            // Crear estela
-            const trail = document.createElement('div');
-            trail.classList.add('trail');
-            trail.style.top = `${parseFloat(star.style.top)}px`;
-            trail.style.left = `${parseFloat(star.style.left)}px`;
-            starField.appendChild(trail);
-
-            // Animar la caída y la estela
-            star.style.animation = `falling ${Math.random() * 3 + 2}s linear infinite`;
-            trail.style.animation = `trail ${Math.random() * 2 + 1}s ease-out infinite`;
-        }
+    function createMeteor() {
+        const meteor = document.createElement('div');
+        meteor.classList.add('meteor');
+        meteor.style.top = `${Math.random() * 100}vh`;
+        meteor.style.left = `${Math.random() * 100}vw`;
+        meteor.style.animationDelay = `${Math.random() * 5}s`;
+        starField.appendChild(meteor);
     }
 
-    // Crear estrellas
-    for (let i = 0; i < numStars; i++) {
-        createStar();
+    for (let i = 0; i < numMeteors; i++) {
+        createMeteor();
     }
 });
